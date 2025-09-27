07faac45a0d0452401f0d36bd07d57b983b93996
Dileepmm12 <dileepwork07@gmail.com>
2025-09-27 17:47:05 +0530

    Delete aiagents directory

--- aiagents/src/main/java/com/example/aiagents/repository/AiAgentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aiagents.repository;
-
-import com.example.aiagents.entity.AiAgent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AiAgentRepository extends JpaRepository<AiAgent, Long> {
-}
